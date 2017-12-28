cfa66d787e3b70e486acae6d5f2c559121189720
ImSane2 <a.maciejaszek@gmail.com>
2017-12-28 21:32:31 +0100

    Switched star system

--- src/app/sections/skills/skill-item/skill-item.component.ts
+++ src/app/sections/skills/skill-item/skill-item.component.ts
@@ -22,13 +22,18 @@ export class SkillItemComponent implements OnInit {
     const prof = this.skillItem.proficiency / 2;
     if (this.skillItem.proficiency % 2 === 0) {
       for (let i = 0; i < prof; i++) {
-        this.starsArray.push('star--full');
+        this.starsArray.push('star fa fa-star star--full');
       }
     } else {
       for (let i = 1; i < prof; i++) {
-        this.starsArray.push('star--full');
+        this.starsArray.push('star fa fa-star star--full');
       }
-        this.starsArray.push('star--half');
+        this.starsArray.push('star fa fa-star-half-o star--half');
+    }
+    if (this.starsArray.length < 5) {
+      for (let i = this.starsArray.length; i < 5; i++) {
+        this.starsArray.push('star--b fa fa-star-o');
+      }
     }
 }
 
