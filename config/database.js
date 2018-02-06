2f21747a2f033f9344e2805a04667f68bc7a5a25
ImSane2 <a.maciejaszek@gmail.com>
2018-02-06 14:06:55 +0100

    credentials

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
     // database: 'mongodb://localhost/cvDB',
-    database: 'mongodb://amaciejaszek:nicoleluch@ds223578.mlab.com:23578/amaciejaszek',
+    // database: 'mongodb://amaciejaszek:nicoleluch@ds223578.mlab.com:23578/amaciejaszek',
+    database: process.env.MONGODB_URI,
     secret: 'secret'
 }
